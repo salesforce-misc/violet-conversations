3c319859f2d0d1a36c026673675c4bece8ae5d0e
Vineet Sinha <vineet.sinha@salesforce.com>
2017-03-03 08:58:45 -0500

    triyng to add more interactivity

--- apps/einstein/index.js
+++ apps/einstein/index.js
@@ -5,7 +5,9 @@ var app = new alexa.app( 'einstein' );
 
 
 app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
+	response
+      .say( 'Welcome to Einstein. What is your name and how old are you?' )
+      .shouldEndSession( false );
 } );
 
 
@@ -28,18 +30,35 @@ app.intent("welcome", {
   function(request, response) {
     var name = request.slot('name');
     var age = request.slot('age');
+    session.getSession().set('age', age);
     response.say("Welcome " + name + " I will remember that you are " + age);
+  }
+);
+
+app.intent("myAge", {
+    "utterances": [
+      "how old am I",
+      "do you know my age",
+      "what is my age"
+    ]
+  },
+  function(request, response) {
+    var age = request.getSession().get('age');
+    if (age)
+      response.say("I remember you telling me that you are " + age);
+    else
+      response.say("I do not know your age.");
   }
 );
 
 app.intent('saySpecificNumber',
   {
     "slots":{"number":"NUMBER"},
-       "utterances":[
-               "say the number {1-100|number}",
-      "give me the number {1-100|number}",
-               "tell me the number {1-100|number}",
-               "I want to hear you say the number {1-100|number}"]
+    "utterances":[
+       "say the number {1-100|number}",
+       "give me the number {1-100|number}",
+       "tell me the number {1-100|number}",
+       "I want to hear you say the number {1-100|number}"]
   },
   function(request,response) {
     var number = request.slot('number');
@@ -51,11 +70,11 @@ function getRandomInt(min, max) {
 }
 app.intent('sayRandomNumber',
   {
-         "utterances":[
-               "say a random number",
+    "utterances":[
+      "say a random number",
       "give me a random number",
-               "tell me a random number",
-               "I want to hear you say a random number"]
+      "tell me a random number",
+      "I want to hear you say a random number"]
   },
   function(request,response) {
     response.say("A random number that you asked for is " + getRandomInt(0,100));

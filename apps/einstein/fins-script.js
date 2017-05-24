c4e4c3b1d667f12c4ebda870eb88781bdc8718b1
Vineet Sinha <vineet.sinha@salesforce.com>
2017-05-23 22:52:59 -0400

    support for the new fins script

---
+++ apps/einstein/fins-script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var alexa = require( 'alexa-app' );
+var app = new alexa.app( 'einstein' );
+var violet = require('../../lib/violet.js')(app);
+var request = require('request');
+
+
+violet.addKeyTypes({
+});
+
+violet.addPhraseEquivalents([
+  ["Do you know", "What is"],
+  ["say", "give", "give me", "tell", "tell me"]
+]);
+
+
+var postToHaiku = function(showValue) {
+  var options = { method: 'POST',
+    url: 'https://dalai-lama-core.herokuapp.com/api/rebound',
+    headers: { 'content-type': 'application/json' },
+    body: { type: 'customapi', title: 'Einstein', value: showValue },
+    json: true };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+}
+
+violet.respondTo([
+      "Can you show me all",
+      "Einstein, Can you show me all"
+    ], function(respond) {
+    respond(["Sure", "Done"]);
+    postToHaiku("all");
+});
+
+violet.respondTo([
+      "Can you show me by category",
+      "Einstein, Can you show me by category"
+    ], function(respond) {
+    respond(["Sure", "Done"]);
+    postToHaiku("by category");
+});
+
+violet.respondTo([
+      "Can you show me by amount",
+      "Einstein, Can you show me by amount"
+    ], function(respond) {
+    respond(["Sure", "Done"]);
+    postToHaiku("by amount");
+});
+
+violet.respondTo([
+      "Can you show me recommendations",
+      "Einstein, Can you show me recommendations"
+    ], function(respond) {
+    respond(["Sure", "Done"]);
+    postToHaiku("recommendations");
+});
+
+violet.respondTo([
+      "I love you",
+      "Einstein, I love you"
+    ], function(respond) {
+    respond("I know");
+});
+
+module.exports = app;

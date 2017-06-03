f1615309680686a64fe9abfe10fa7b6813b4cfcb
Vineet Sinha <vineet.sinha@salesforce.com>
2017-06-03 17:57:48 -0400

    implementation of apiai documentation to show how it would look like with the new conversational api

---
+++ apps/einstein/sample-apiai-script.js
@@ -0,0 +1,121 @@
+'use strict';
+
+var alexa = require('alexa-app');
+var app = new alexa.app('einstein');
+var violet = require('../../lib/violet.js')(app);
+
+// Implementing https://docs.api.ai/docs/dialogs
+
+violet.addKeyTypes({
+  '((destination))': 'AMAZON.US_CITY',
+  '((checkinDate))': 'AMAZON.DATE',
+  '((checkoutDate))': 'AMAZON.DATE',
+});
+
+
+violet.addPhraseEquivalents([
+]);
+
+
+violet.respondTo({
+  expecting: ['Book a hotel in ((destination)) check in ((checkinDate)) check out ((checkoutDate))'],
+  resolve: (response) => {
+    addGoal('{{bookHotel}}');
+  }
+});
+
+violet.meetGoal({
+  goal: '{{bookHotel}}',
+  resolve: (response) => {
+    if (!response.goalFilled('{{checkinDate}}', '((checkinDate))')
+        || !response.goalFilled('{{checkoutDate}}', '((checkoutDate))')
+        || !response.goalFilled('{{destination}}', '((destination))') ) {
+          return false; // dependent goals not met
+        }
+    var checkinDate = response.get('{{checkinDate}}');
+    var checkoutDate = response.get('{{checkoutDate}}');
+    var destination = response.get('{{destination}}');
+    hotelReservationService.getReservationInfo(checkinDate, checkoutDate, destination, (numNights, dailyRate)=>{
+      response.set('{{numNights}}', numNights);
+      response.set('{{dailyRate}}', dailyRate);
+      response.addGoal('{{confirmBooking}}');
+      response.ask('Book hotel room at ' + destination + ' checking in on ' + checkinDate + ' for ' + numNights ' at ' + dailyRate + ' dollars per night');
+      // response.say('Reserved hotel room');
+    });
+    return true;
+  }
+});
+
+
+violet.meetGoal({
+  goal: '{{checkinDate}}',
+  prompt: ['What date', 'When are you arriving?'],
+});
+violent.respondTo({
+    expecting: ['{{checkinDate}}', '((checkinDate))'],
+    resolve: (response) => {
+      response.set('{{checkinDate}}', response.get('((checkinDate))') );
+    }
+});
+
+
+violet.meetGoal({
+  goal: '{{checkoutDate}}',
+  prompt: 'xxxx xxxx',
+});
+violent.respondTo({
+    expecting: ['{{checkoutDate}}', '((checkoutDate))'],
+    resolve: (response) => {
+      response.set('{{checkoutDate}}', response.get('((checkoutDate))') );
+    }
+});
+
+
+violet.meetGoal({
+  goal: '{{destination}}',
+  prompt: 'xxxx xxxx',
+});
+violent.respondTo({
+    expecting: ['{{destination}}', '((destination))'],
+    resolve: (response) => {
+      response.set('{{destination}}', response.get('((destination))') );
+    }
+});
+
+
+violet.meetGoal({
+  goal: '{{confirmBooking}}',
+  prompt: 'Book hotel room at {{destination}} checking in on {{checkinDate}} for {{numNights}} at {{dailyRate}} dollars per night'
+});
+
+
+violet.respondTo({
+  expecting: ['{{confirmBooking}}', 'Yes'],
+  resolve: (response) => {
+    hotelReservationService.reserve(checkinDate, checkoutDate, destination, (numNights, dailyRate)=>{
+      response.say('Reserved hotel room');
+    });
+  }
+});
+
+violet.respondTo({
+  expecting: ['{{confirmBooking}}', ['No', 'Cancel']],
+  resolve: (response) => {
+  }
+});
+
+violet.respondTo({
+  expecting: ['{{confirmBooking}}', 'Change check in date'],
+  resolve: (response) => {
+    response.addGoal('{{checkinDate}}');
+  }
+});
+
+violet.respondTo({
+  expecting: ['{{confirmBooking}}', 'Yes'],
+  resolve: (response) => {
+    hotelReservationService.reserve(checkinDate, checkoutDate, destination, (numNights, dailyRate)=>{
+      response.say('Reserved hotel room');
+    });
+  }
+});

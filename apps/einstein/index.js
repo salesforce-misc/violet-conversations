3bf964cbd4a3362eb816b83fe822e09e89e779de
Vineet Sinha <vineet.sinha@salesforce.com>
2017-06-03 23:06:39 -0400

    working on the diabetes script

--- apps/einstein/index.js
+++ apps/einstein/index.js
@@ -1,3 +1,4 @@
 
-module.exports = require('./demo1-script.js');
+// module.exports = require('./demo1-script.js');
 //module.exports = require('./fins-script.js');
+module.exports = require('./hls-diabetes-script.js');

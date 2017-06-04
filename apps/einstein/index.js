7b9caa728a425fe1f0206cd9d4b2969d654f4282
Vineet Sinha <vineet.sinha@salesforce.com>
2017-06-03 23:28:54 -0400

    also include the sample tutorial for testing

--- apps/einstein/index.js
+++ apps/einstein/index.js
@@ -1,4 +1,5 @@
 
 // module.exports = require('./demo1-script.js');
-//module.exports = require('./fins-script.js');
+// module.exports = require('./fins-script.js');
+// module.exports = require('./sample-tutorial-script.js');
 module.exports = require('./hls-diabetes-script.js');

1ac4f5dfe7b8dfb7b6fa47723465912598461628
Gina Nguyen <gina.nguyen@ginanguyen-ltm.internal.salesforce.com>
2017-06-14 17:11:58 -0700

    removed branch changes

--- apps/einstein/index.js
+++ apps/einstein/index.js
@@ -2,4 +2,4 @@
 // module.exports = require('./demo1-script.js');
 // module.exports = require('./fins-script.js');
 // module.exports = require('./sample-tutorial-script.js');
-module.exports = require('./bathroom-script.js');
+module.exports = require('./hls-diabetes-script.js');

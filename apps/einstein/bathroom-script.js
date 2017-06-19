2f4dcce7e95c32b85bec2863c87bfd24ade691b0
Gina Nguyen <gina.nguyen@ginanguyen-ltm.internal.salesforce.com>
2017-06-19 14:32:16 -0400

    updated to dynamically use name

--- apps/einstein/bathroom-script.js
+++ apps/einstein/bathroom-script.js
@@ -19,7 +19,7 @@ violet.addPhraseEquivalents([
 violet.respondTo({
   expecting: ['When is my appointment with [[doctor]]?', 'When do I see [[doctor]] next', 'Do I have an upcoming appointment with [[doctor]]'],
   resolve: (response) => {
-   response.say('Your next appointment is in 3 days, on Thursday at 1130 AM.')
+    response.say('Your next appointment with ' + response.get('[[doctor]]') + ' is in 3 days, on Thursday at 1130 AM.')
 }});
 
 module.exports = app;

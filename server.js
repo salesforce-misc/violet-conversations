68a6a31182054d3bed28ae2fba7a8331be2e305a
Vineet Sinha <vineet.sinha@salesforce.com>
2017-07-11 19:23:59 -0400

    server functionality moving to violetSrvr.js

--- server.js
+++ server.js
@@ -1,40 +1,18 @@
 'use strict';
 
-var path = require('path');
-var express = require('express');
-var http = require('http');
-var ws = require('ws');
+var violetSrvr = require('./lib/violetSrvr.js')('/alexa');
+var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
 
-var expressApp = express();
-var alexaRouter = express.Router();
-expressApp.set('view engine', 'ejs');
-expressApp.set('views', path.join(__dirname, 'tester-views'));
-expressApp.use(express.static(path.join(__dirname, 'tester-views')));
-expressApp.use('/alexa', alexaRouter);
+violetSrvr = require('./lib/violetClientTx.js')(violetSrvr, srvrInstance);
 
-var srvrInstance = http.createServer(expressApp);
-srvrInstance.listen(process.env.PORT || 8080);
+// violetSrvr.loadScript('./examples/demo1.js');
+// violetSrvr.loadScript('./examples/tutorial.js');
+// violetSrvr.loadScript('./scripts/fins.js');
+// violetSrvr.loadScript('./scripts/hls-diabetes.js');
+// violetSrvr.loadScript('./scripts/leadsAndOpportunities.js');
+// violetSrvr.loadScript('./scripts/cases-customer.js');
+// violetSrvr.loadScript('./scripts/cases-employee.js');
+violetSrvr.loadScript('../scripts/knowledge-base.js');
 
-// var script = require('./examples/demo1.js');
-// var script = require('./examples/tutorial.js');
-// var script = require('./scripts/fins.js');
-var script = require("./scripts/hls-diabetes.js");
-script.setServerApp(express, alexaRouter);
-
-var wss = new ws.Server({ server: srvrInstance  });
 
 console.log('Waiting for requests...');
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-function broadcast(jsonObj) {
-  console.log('Broadcasting...', jsonObj);
-  wss.clients.forEach((client) => {
-    client.send(JSON.stringify(jsonObj));
-  });
-}
-
-script.setBroadcaster(broadcast);

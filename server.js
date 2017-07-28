95cc4108fb455e0d2e1866152bc26a884988e7e4
Vineet Sinha <vineet.sinha@salesforce.com>
2017-07-28 10:34:14 -0400

    making sure uncommenting will be enough to get these running

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
 
 violetSrvr = require('./lib/violetClientTx.js')(violetSrvr, srvrInstance);
 
-// violetSrvr.loadScript('./examples/demo1.js');
-// violetSrvr.loadScript('./examples/tutorial.js');
-// violetSrvr.loadScript('./scripts/fins.js');
-// violetSrvr.loadScript('./scripts/hls-diabetes.js');
-// violetSrvr.loadScript('./scripts/leadsAndOpportunities.js');
-// violetSrvr.loadScript('./scripts/cases-customer.js');
-// violetSrvr.loadScript('./scripts/cases-employee.js');
+// violetSrvr.loadScript('../examples/demo1.js');
+// violetSrvr.loadScript('../examples/tutorial.js');
+// violetSrvr.loadScript('../scripts/fins.js');
+// violetSrvr.loadScript('../scripts/hls-diabetes.js');
+// violetSrvr.loadScript('../scripts/leadsAndOpportunities.js');
+// violetSrvr.loadScript('../scripts/cases-customer.js');
+// violetSrvr.loadScript('../scripts/cases-employee.js');
 violetSrvr.loadScript('../scripts/knowledge-base.js');
 
 

2a69776f47594a4b63c3f09a66d816a267c83407
Ashita Saluja <asaluja@asaluja-ltm2.internal.salesforce.com>
2017-07-31 13:05:25 -0700

    New script to help users find VTO activities

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var violetSrvr = require('./lib/violetSrvr.js')('/alexa');
 var srvrInstance = violetSrvr.createAndListen(process.env.PORT || 8080);
 
 violetSrvr = require('./lib/violetClientTx.js')(violetSrvr, srvrInstance);
-
+violetSrvr.loadScript('../scripts/sf-VF-VTO.js');
 // violetSrvr.loadScript('../examples/demo1.js');
 // violetSrvr.loadScript('../examples/tutorial.js');
 // violetSrvr.loadScript('../scripts/sfxi-fins.js');
 // violetSrvr.loadScript('../scripts/hls-diabetes.js');
-violetSrvr.loadScript('../scripts/sf-leadsAndOpportunities.js');
+//violetSrvr.loadScript('../scripts/sf-leadsAndOpportunities.js');
 // violetSrvr.loadScript('../scripts/sf-cases-customer.js');
 // violetSrvr.loadScript('../scripts/sf-cases-employee.js');
 // violetSrvr.loadScript('../scripts/sf-knowledge-base.js');

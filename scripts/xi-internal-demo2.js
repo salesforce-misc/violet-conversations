630bc440e2e99b45ac14eab7673bf82e54e09aac
Vineet Sinha <vineet.sinha@salesforce.com>
2017-09-05 12:30:48 -0400

    updating script to meet Johns updates

--- scripts/xi-internal-demo2.js
+++ scripts/xi-internal-demo2.js
@@ -23,36 +23,35 @@ var postToThrive = function() {
 violet.setSpokenRate('fast');
 
 violet.respondTo([
+      "Whats up"
       "{Are|} you there",
       "{Do you|} have a moment"
     ], (response) => {
+    // XXX - Query First Record - http://haiku-core-parker.herokuapp.com/api/records/Forcebit_Order__c
     response.say(["Absolutely.  How can I help?", "Yep.  How can I help?", "I am"]);
 });
 
 violet.respondTo([
-      "How did you decide what recommendations to show"
+      "Definitely, that would be great"
     ], (response) => {
     postToThrive();
     response.say([`
-      Great question.  Take a peek at this decision tree that I thought up based
-      on my understanding of the customer.
+      Great - here it is.
       `]);
 });
 
 violet.respondTo([
       "{Perfect|} {Wait|} was this a conversation",
     ], (response) => {
-    response.say([`Very astute observation John - Alexa and I are good buddies,
-      but successful conversations are all about knowing the customer.  This is
-      an example of us working together.`]);
+    response.say([`Very astute observation John.`]);
 });
 
-// violet.respondTo([
-//       "Alexa doesn't do that {though|}",
-//     ], (response) => {
-//     postToThrive();
-//     response.say(["Think of Alexa and I as good buddies - we just work together to be better.  I also natively talk to Salesforce through scripts similar to this one."]);
-// });
+violet.respondTo([
+      "But Alexa doesn't do that {though|}",
+    ], (response) => {
+    postToThrive();
+    response.say(["Alexa and I as good buddies, but successful conversations are all about knowing the customer.  This is an example of us working together."]);
+});
 
 violet.respondTo([
       "{Perfect|Wow} {That is great|} Thanks {for sharing|}",

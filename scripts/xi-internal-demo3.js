8c28bb1897a04d140be0724558a6a0117fa81881
Vineet Sinha <vineet.sinha@salesforce.com>
2017-10-03 10:53:06 -0400

    errors and polish for demo

--- scripts/xi-internal-demo3.js
+++ scripts/xi-internal-demo3.js
@@ -12,14 +12,15 @@ var Promise = require('bluebird');
 violet.respondTo([
       "what floor is Salesforce located on"
     ], (response) => {
-    response.say('The 30th to 36th. Check in as at the front desk on this floor');
+    response.say('Salesforce is on the 30th to 36th. Check in as at the front desk on this floor');
+    response.endConversation();
 });
 
 violet.respondTo([
       "how can I interact with the displays"
     ], (response) => {
-    showCodeBlock();
     response.say(['Text hello to 415 941-5532']);
+    response.endConversation();
 });
 
 module.exports = violet;

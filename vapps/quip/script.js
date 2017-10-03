535f50e59ef3ef0f47731bd8b437c31f8d0ba627
Vineet Sinha <vineet.sinha@salesforce.com>
2017-10-02 21:21:38 -0400

    allowing to check an item previously mentioned

--- vapps/quip/script.js
+++ vapps/quip/script.js
@@ -51,10 +51,25 @@ violet.respondTo(['whats next on my to do'],
         for (var i of items) {
           if (i.done == false) {
             response.set('tgtItemId', i.id);
+            response.set('tgtItemHtml', i.html);
+            response.set('tgtItemText', i.text);
             response.say(`The next item is ${i.text}`);
             return;
           }
         }
         response.say(`There are no items that need to be done on your list`);
       });
+});
+
+violet.respondTo(['mark item as checked'],
+  (response) => {
+    var tgtItemId = response.get('tgtItemId');
+    var tgtItemHtml = response.get('tgtItemHtml');
+    var tgtItemText = response.get('tgtItemText');
+    if (tgtItemId && tgtItemHtml) {
+      response.say(`Marking ${tgtItemText} as done`);
+      // TODO: implement this correctly
+      return quipSvc.modifyListItem(tgtDocId, tgtItemId, [`<del>${tgtItemHtml}</del>`]);
+    } else
+      response.say(`Which item are you referring to`);
 });

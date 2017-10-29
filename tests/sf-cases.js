b7a64a2cc605517fdc5a0a112946b9cfeb10aeaa
Vineet Sinha <vineet.sinha@salesforce.com>
2017-10-28 23:56:28 -0400

    adding license header

--- tests/sf-cases.js
+++ tests/sf-cases.js
@@ -1,3 +1,6 @@
+/* Copyright (c) 2017-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
 
 var violet = require('../lib/violet.js').script();
 var violetTime = require('../lib/violetTime.js')(violet);

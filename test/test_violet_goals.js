29b7c6ef7360a96bbcaba7b7f0937a1e2ec3f2b9
Vineet Sinha <vineet.sinha@salesforce.com>
2017-11-27 07:10:23 -0500

    more goal mechanich testing and documentation

--- test/test_violet_goals.js
+++ test/test_violet_goals.js
@@ -45,4 +45,69 @@ describe('violet goals', function() {
 
   });
 
+  var basicPromptGoalDef = (violet)=>{
+    vh.violet.respondTo('Hello', (response) => {
+      response.say('Hi');
+      response.addGoal('welcome');
+    });
+    vh.violet.defineGoal({
+      goal: 'welcome',
+      prompt: 'How are you doing',
+      respondTo: [{
+        expecting: 'doing well',
+        resolve: (response) => {
+          response.say('Glad to hear that');
+      }},{
+        expecting: 'not well',
+        resolve: (response) => {
+          response.say('Sorry to hear that');
+      }}]
+    });
+  }
+
+  describe('defineGoal mechanics', function() {
+
+    /*
+    // TODO: needs to be spec'd out
+    it('intents inside a defined goal to ?not? get triggered by themselves', function() {
+      basicPromptGoalDef(vh.violet);
+      vh.initialize();
+      return vh.sendIntent('doing well').then(({rcvdStr, body})=>{
+        assert.notEqual('Glad to hear that', rcvdStr);
+      });
+    });
+    */
+
+    it('once a goal is set it triggers a prompt from a defined goal', function() {
+      basicPromptGoalDef(vh.violet);
+      vh.initialize();
+      return vh.sendIntent('Hello').then(({rcvdStr, body})=>{
+        assert.equal('Hi <break time="500ms"/> How are you doing', rcvdStr);
+      });
+    });
+
+    it('once a goal is set you can trigger nested intents', function() {
+      basicPromptGoalDef(vh.violet);
+      vh.initialize();
+      return vh.sendIntent('Hello').then(({rcvdStr, body})=>{
+        return vh.sendIntent('doing well').then(({rcvdStr})=>{
+          assert.equal('Glad to hear that', rcvdStr);
+        });
+      });
+    });
+
+    // TODO: adapt below for goals
+
+    // it('once a goals is set and it is cleared then goal prompts are not triggered', function() {
+    // });
+
+    // it('multiple goals will mean prompts get triggered from the first goal first', function() {
+    // });
+
+    // it('goals that get triggered without a prompt will have their resolve being called', function() {
+    // });
+
+
+  });
+
 });

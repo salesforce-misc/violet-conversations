e9c48b9bd5388819ef9a2efdc0657b55159b1832
Vineet Sinha <vineet.sinha@salesforce.com>
2017-12-03 00:18:10 -0500

    tests for the pg plugin

---
+++ test/test_violetStorePG.js
@@ -0,0 +1,102 @@
+var co = require('co');
+var assert = require('assert');
+var vh = require('./violetHelper.js');
+
+describe('violetStorePG', function() {
+  // this.timeout(5*1000);
+  // before(function(){
+  //   return
+  // })
+
+  var initDB = (pgClient) => {
+    // check if the type has been created
+    return pgClient.query("select * from pg_type where typname = 'states'").then((results)=>{
+      // create type if needed
+      if (results.rows.length==0)
+        return violetStorePG.client.query("create type states as enum ('New', 'Waiting', 'Running', 'Failed', 'Passed')");
+      else
+        return Promise.resolve();
+    }).then(()=>{
+      // create table if needed
+      return pgClient.query(`
+        create table if not exists Automated_Tests
+          (Id serial primary key, Name text, Status States, Verified boolean)
+        `);
+    });
+  }
+
+
+  describe('query multiple ways', function() {
+
+    it('should be able to do a query using basic query parameters', function() {
+      var violetStorePG = require('../lib/violetStorePG')(vh.violet);
+      violetStorePG.store.propOfInterest = {
+        'Automated_Tests': ['Name', 'Status', 'Verified']
+      };
+      vh.violet.respondTo('Hello', function* (response) {
+        response.say('Hi');
+        var recName = `Important Record: ${Math.round(Math.random()*1000*1000)}`
+        // var results = yield response.load('Automated_Tests', 'Status', 'New');
+        // console.log('results: ', results);
+        // assert.ok(Array.isArray(results));
+
+        // Create
+        yield response.store('Automated_Tests', {
+          'Name': recName,
+          Status: 'New',
+          Verified: true
+        });
+        // Read
+        var results = yield response.load({
+          objName: 'Automated_Tests',
+          keyName: 'Name',
+          keyVal: recName
+        });
+        console.log('results: ', results);
+        assert.ok(Array.isArray(results));
+        assert.ok(results.length==1);
+        assert.equal(results[0].name,recName);
+
+        // Update
+        yield response.update('Automated_Tests', 'Name', recName, {'Status': 'Running'});
+        results = yield response.load({
+          objName: 'Automated_Tests',
+          keyName: 'Name',
+          keyVal: recName
+        });
+        // console.log('results: ', results);
+        assert.ok(Array.isArray(results));
+        assert.ok(results.length==1);
+        assert.equal(results[0].status,'Running');
+
+        // Delete
+        yield response.delete('Automated_Tests', 'Name', recName, {'Status': 'Running'});
+        results = yield response.load({
+          objName: 'Automated_Tests',
+          keyName: 'Name',
+          keyVal: recName
+        });
+        // console.log('results: ', results);
+        assert.ok(Array.isArray(results));
+        assert.ok(results.length==0);
+
+      });
+      vh.initialize();
+      return initDB(violetStorePG.client).then(()=>{
+        return vh.sendIntent('Hello');
+      }).then(({rcvdStr, body})=>{
+        console.log('Received: ' + rcvdStr);
+        assert.equal('Hi', rcvdStr);
+
+        violetStorePG.client.end();
+      }).catch(err=>{
+        console.log('err', err, err.stack)
+      });
+
+
+    });
+
+  });
+
+
+});

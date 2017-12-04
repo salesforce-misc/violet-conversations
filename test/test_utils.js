bab83a9f56eaa206f6ecf96af5bd4ed953d7bf77
Vineet Sinha <vineet.sinha@salesforce.com>
2017-12-04 10:51:16 -0500

    more coverage in tests

--- test/test_utils.js
+++ test/test_utils.js
@@ -8,12 +8,19 @@ describe('utils', function() {
   });
 
   it('getNumAsStr should be able to convert numbers', function() {
+    assert.equal(utils.getNumAsStr(-5),  'err');
+    assert.equal(utils.getNumAsStr(0),   'zero');
+    assert.equal(utils.getNumAsStr(11),  'eleven');
+    assert.equal(utils.getNumAsStr(17),  'seventeen');
     assert.equal(utils.getNumAsStr(23),  'twenty three');
     assert.equal(utils.getNumAsStr(41),  'forty one');
     assert.equal(utils.getNumAsStr(162), 'one hundred sixty two');
+    assert.equal(utils.getNumAsStr(210), 'two hundred ten');
     assert.equal(utils.getNumAsStr(390), 'three hundred ninety');
+    assert.equal(utils.getNumAsStr(413), 'four hundred thirteen');
     assert.equal(utils.getNumAsStr(732), 'seven hundred thirty two');
     assert.equal(utils.getNumAsStr(999), 'nine hundred ninety nine');
+    assert.equal(utils.getNumAsStr(1024),'err');
   });
 
 
